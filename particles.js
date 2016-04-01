81c39b8eb24c6fdccd975f8deefea78ee297f20e
Marcin Rudzki <marcin.rudzki@ericsson.com>
2016-04-01 16:02:45 +0200

    Creating the colorful particles mob

--- particles.js
+++ particles.js
@@ -1,22 +1,55 @@
 var canvas = document.querySelector('.particles');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 var ctx = canvas.getContext('2d');
 
-var particles = [
-    { x: 50, y: 50, r: 20, color: 'red' },
-    { x: 150, y: 20, r: 10, color: 'green' },
-    { x: 100, y: 150, r: 50, color: 'blue' }
-];
+var ParticleMob = function(){
+    this._particles = [];
+    for (var i = 0; i < 50; i++) {
+        this._particles.push(new Particle());
+    }
+};
 
-particles.forEach(function (particle) {
-    draw(particle);
-});
+ParticleMob.prototype.tick = function() {
+    this._particles.forEach(function(particle) {
+        particle.draw();
+        particle.tick();
+    });
+};
 
-function draw(particle) {
-    ctx.beginPath();
-    ctx.fillStyle = particle.color;
-    ctx.arc(particle.x, particle.y, particle.r, 0, Math.PI * 2);
-    ctx.fill();
+var Particle = function() {
+    this.init();
+};
+
+function randomColour(){
+    return Math.floor(Math.random() * 255)+',';
 }
+
+Particle.prototype.init = function() {
+    this._color = 'rgba('+randomColour()+randomColour()+randomColour() +  '0.5)';
+    this._x = Math.random() * canvas.width;
+    this._y = Math.random() * canvas.height;
+    this._radius = 50 + Math.random() * 50;
+};
+
+Particle.prototype.draw = function() {
+    ctx.fillStyle = this._color;
+    ctx.beginPath();
+    ctx.arc(this._x, this._y, this._radius, 0, Math.PI * 2);
+    ctx.fill();
+};
+
+Particle.prototype.tick = function() {
+    if (this._radius < 2) {
+        this.init();
+    }
+    this._radius -= Math.random();
+};
+
+var mob = new ParticleMob();
+
+(function animation(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    mob.tick();
+    window.requestAnimationFrame(animation);
+})();
