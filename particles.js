d239c6315b079cfbbff16313d9f5ca5c74b10421
Marcin Rudzki <mar.rudzki@gmail.com>
2017-04-22 20:44:10 +0200

    Extract ParticlesMob and Randomizer classes

--- particles.js
+++ particles.js
@@ -4,21 +4,19 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+class Randomizer {
+    static getRandomColor() {
+        let rgb = 'rgba(', i;
+        for (i = 0; i < 3; i++) {
+            rgb += Math.floor(Math.random() * 255) + ', ';
+        }
+        return rgb + '0.5)';
+    }
+}
+
 class Particle {
     constructor() {
         this.randomize();
-    }
-    draw(ctx) {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-    }
-    randomize() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.radius = 50 + Math.random() * 50;
-        this.color = getRandomColor();
     }
     tick() {
         if (this.radius < 2) {
@@ -26,35 +24,39 @@ class Particle {
         }
         this.radius -= Math.random();
     }
+    randomize() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.radius = 50 + Math.random() * 50;
+        this.color = Randomizer.getRandomColor();
+    }
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+    }
 }
 
-
-var ParticleMob = function(){
-    this._particles = [];
-    for (var i = 0; i < 50; i++) {
-        this._particles.push(new Particle());
+class ParticlesMob {
+    constructor() {
+        this.particles = [];
+        for (let i = 0; i < 50; i++) {
+            this.particles.push(new Particle());
+        }
     }
-};
+    tick() {
+        this.particles.forEach(particle => {
+            particle.draw(ctx);
+            particle.tick();
+        });
+    }
+}
 
-var mob = new ParticleMob();
-
-ParticleMob.prototype.tick = function() {
-    this._particles.forEach(function(particle) {
-        particle.draw(ctx);
-        particle.tick();
-    });
-};
+const mob = new ParticlesMob();
 
 (function animationLoop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     mob.tick();
     window.requestAnimationFrame(animationLoop);
 })();
-
-function getRandomColor() {
-    let rgb = 'rgba(', i;
-    for (i = 0; i < 3; i++) {
-        rgb += Math.floor(Math.random() * 255) + ', ';
-    }
-    return rgb + '0.5)';
-}
